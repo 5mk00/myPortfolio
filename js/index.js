a2fe52e09ac350a3c75269a5ad6fd33ec1293edb
5mk00 <omk000915@gmail.com>
2025-10-09 19:40:31 +0900

    25.10.09

--- js/index.js
+++ js/index.js
@@ -27,8 +27,53 @@ function designMoreBtn(){
 
 function designShow(){
     $('#design ul li').click(function(){
-        var imgSrc = $(this).find('img').attr('src');
-        $('#designShow').find('img').attr('src', imgSrc);
+        $('#designShow figure').find('img').detach();
+        var className=$(this).hasClass("designMore");
+        if(className==false){
+            var imgSrc = $(this).find('img').attr('src');
+            $('#designShow figure').append('<img src="'+imgSrc+'">');
+            
+        }
+        else if(className==true){
+            var imgSrc = 'images/'+$(this).find('figure').attr('class')+'_1.jpg';
+            $('#designShow').find('img').attr('src', imgSrc);
+            if($(this).find('figure').hasClass('design_detail01')==true){
+                $('#designShow figure').append('<img src="images/design_detail01_1.jpg" alt="상세페이지 디자인" class="designDetail">');
+                $('#designShow figure').append('<img src="images/design_detail01_2.jpg" alt="상세페이지 디자인" class="designDetail">');
+                $('#designShow figure').append('<img src="images/design_detail01_3.jpg" alt="상세페이지 디자인" class="designDetail">');
+                $('#designShow figure').append('<img src="images/design_detail01_4.jpg" alt="상세페이지 디자인" class="designDetail">');
+                $('#designShow figure').append('<img src="images/design_detail01_5.jpg" alt="상세페이지 디자인" class="designDetail">');
+                $('#designShow figure').append('<img src="images/design_detail01_6.gif" alt="상세페이지 디자인" class="designDetail">');
+                $('#designShow figure').append('<img src="images/design_detail01_7.jpg" alt="상세페이지 디자인" class="designDetail">');
+                $('#designShow figure').append('<img src="images/design_detail01_8.jpg" alt="상세페이지 디자인" class="designDetail">');
+            }
+            else if($(this).find('figure').hasClass('design_SNS01')==true){
+                $('#designShow figure').append('<img src="images/design_SNS01_1.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS01_2.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS01_3.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS01_4.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS01_5.jpg" alt="SNS 디자인" class="designSNS">');
+            }
+            else if($(this).find('figure').hasClass('design_SNS02')==true){
+                $('#designShow figure').append('<img src="images/design_SNS02_1.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS02_2.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS02_3.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS02_4.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS02_5.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS02_6.jpg" alt="SNS 디자인" class="designSNS">');
+            }
+            else if($(this).find('figure').hasClass('design_SNS03')==true){
+                $('#designShow figure').append('<img src="images/design_SNS03_1.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS03_2.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS03_3.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS03_4.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS03_5.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS03_6.jpg" alt="SNS 디자인" class="designSNS">');
+                $('#designShow figure').append('<img src="images/design_SNS03_7.jpg" alt="SNS 디자인" class="designSNS">');
+            }
+        }
+
+
         var imgName = $(this).find('figcaption').text();
         if(imgName=="배너"){
             var imgName = "Banner(배너)";
@@ -38,6 +83,9 @@ function designShow(){
         }
         else if(imgName=="포스터"){
             var imgName = "ETC(포스터)";
+        }
+        else if(imgName=="SNS"){
+            var imgName = "SNS(광고)";
         }
         else if(imgName=="패키지"){
             var imgName = "Package(패키지)";
@@ -78,6 +126,11 @@ function designNav(designNav){
             designImgBoxHeight = $(this).parents("#design").children(".designBox").innerHeight();
         }
         else if(a==".designDetail"){
+            $('#design ul li').removeClass("active");
+            $(a).parents('li').addClass("active");
+            designImgBoxHeight = $(this).parents("#design").children(".designBox").innerHeight();
+        }
+        else if(a==".designSNS"){
             $('#design ul li').removeClass("active");
             $(a).parents('li').addClass("active");
             designImgBoxHeight = $(this).parents("#design").children(".designBox").innerHeight();
